54ecc7be8ba3d3a29f67cbd8a8250997bae4d1eb
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-05-31 18:30:03 -0700

    error where negative numbers were not getting handled

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,7 @@
 import random
 
 number = random.randint(-10000, 10000)
-number_str = str(number)
-last_digit = number_str[-1]
+last_digit = abs(number) % 10
 
 str_1 = "Last digit of"
 str_2 = "is"
@@ -11,9 +10,12 @@ str_greater_than_5 = "and is greater than 5"
 str_is_0 = "and is 0"
 str_less_than_6_not_0 = "and is less than 6 and not 0"
 
-if int(last_digit) > 5:
-    print(f"{str_1} {number_str} {str_2} {last_digit} {str_greater_than_5}")
-elif int(last_digit) == 0:
-    print(f"{str_1} {number_str} {str_2} {last_digit} {str_is_0}")
-elif int(last_digit) < 6 and int(last_digit) != 0:
-    print(f"{str_1} {number_str} {str_2} {last_digit} {str_less_than_6_not_0}")
+if number < 0:
+    last_digit = -last_digit
+
+if last_digit > 5:
+    print(f"{str_1} {number} {str_2} {last_digit} {str_greater_than_5}")
+elif last_digit == 0:
+    print(f"{str_1} {number} {str_2} {last_digit} {str_is_0}")
+else:
+    print(f"{str_1} {number} {str_2} {last_digit} {str_less_than_6_not_0}")
