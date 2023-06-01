16bd3ccc66ba3b1210d9914692c7d7f914f79085
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-05-31 20:24:09 -0700

    format

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != ord('e') and i != ord('q'):
-        print("{}".format(chr(i)), end="")
+for ascii_value in range(97, 123):
+    if ascii_value != 101 and ascii_value != 113:
+        print("{0}".format(chr(ascii_value)), end="")
