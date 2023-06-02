791362ae7183e80331d62331ba779eacb4e99f7b
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-06-02 02:06:11 -0700

    added function that prints 0 to 99 in double digits

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(100):
+    if i < 10:
+        print("{:02d},".format(i), end=" ")
+    elif i == 99:
+        print("{:02d}".format(i), end="\n")
+    else:
+        print("{:02d},".format(i), end=" ")
