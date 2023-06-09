ded75e422fcd2a93c66c60c32befb67b12f8dd0d
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-06-08 19:23:17 -0700

    error check

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-    for row in matrix:
-        new_row = []
-        for num in row:
-            new_row.append(num ** 2)
-        new_matrix.append(new_row)
+    new_matrix = []
+    for list_in_matrix in matrix:
+        new_list = []
+        for elem in list_in_matrix:
+            new_list.append(elem ** 2)
+        new_matrix.append(new_list)
     return new_matrix
