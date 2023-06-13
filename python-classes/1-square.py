63c765bcd3b6bb33ae0b94c1c7794afda0631b3d
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-06-13 16:21:04 +1000

    Update 1-square.py
    some formatting chnages only

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 
-"""Define a class square.
-This class represents a square and its private attribute
+"""
+Define a class Square.
+
+Represents a square, with its size.
 """
 
 class Square:
     """
-    Class representing square.
+    A class that represents a square.
 
     Attributes:
-    __size: The size of the square.
+        __size (int): The size of the square.
     """
+
     def __init__(self, size):
         """
-        Inititalises a square instance.
+        Initializes a Square instance.
 
         Args:
-        size (int): The size of the square.
+            size (int): The size of the square.
         """
         self.__size = size
