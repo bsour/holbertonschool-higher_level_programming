2bca2a01d6069ec2bf37431496d0d4eb008b7d0b
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-06 15:29:04 +1000

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""
-    6-from_json_string.py
-    Function that writes an Object to \
-    a text file, using a JSON representation.
-"""
-import json
+""" File name : 9-add_item.py"""
+import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """Function that writes an Object to \
-    a text file, using a JSON representation."""
-    with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(json.dumps(my_obj))
+    try:
+        items = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, 'add_item.json')
