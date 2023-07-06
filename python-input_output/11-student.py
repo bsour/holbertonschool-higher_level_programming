9229f730ec77841a5378765004ec2d442e9b446d
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-06 15:37:39 +1000

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,29 +1,47 @@
 #!/usr/bin/python3
-""" File name : 12-student.py
+"""Module 11-student.
+Creates a Student class.
 """
 
 
-class Student(object):
-    """Class student"""
+class Student:
+    """Class that defines a student.
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method to_json().
+    Public method reload_from_json().
+    """
 
     def __init__(self, first_name, last_name, age):
-        """__init__ initialized constructor
+        """Initializes the Student instance."""
 
-        Args:
-            first_name (str): first name
-            last_name (str: last name
-            age (int): age
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary represt """
-        if attrs is None:
-            return self.__dict__
-        dictionary = {}
-        for key, value in self.__dict__.items():
-            if key in attrs:
-                dictionary[key] = value
-        return dictionary
+        """Retrieves a dictionary representation
+        of a Student instance.
+        Args:
+            - attrs: list of attributes
+        Returns: the dict representation of the instance.
+        """
+
+        my_dict = dict()
+        if attrs and all(isinstance(x, str) for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance.
+        Args:
+            - json: dictionnary of attributes
+        """
+
+        for x in json:
+            self.__dict__.update({x: json[x]})
