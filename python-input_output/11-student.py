39ce1e92c87eae4ef7ef9364d22fdf5c26fb6429
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-06 15:30:56 +1000

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""
-    6-from_json_string.py
-    Function that writes an Object to \
-    a text file, using a JSON representation.
+""" File name : 12-student.py
 """
 
 
-class Student:
+class Student(object):
+    """Class student"""
 
     def __init__(self, first_name, last_name, age):
+        """__init__ initialized constructor
+
+        Args:
+            first_name (str): first name
+            last_name (str: last name
+            age (int): age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(obj):
-        """Function that writes an Object to \
-        a text file, using a JSON representation."""
-        return obj.__dict__
+    def to_json(self, attrs=None):
+        """retrieves a dictionary represt """
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dictionary[key] = value
+        return dictionary
