3943ef4280685d665150496b1b92e0fe11f703d3
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-25 06:13:50 -0700

    added task 4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Write a script that creates a table called first_table in the current
+-- database
+-- coloumn ID and name
+CREATE TABLE IF NOT EXISTS first_table (
+       id int,
+       name VARCHAR9256),
+);
