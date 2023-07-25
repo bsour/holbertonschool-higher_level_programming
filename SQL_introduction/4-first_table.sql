abfcbf152a289634752fa5df7c69dd23a72cea05
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-25 06:16:05 -0700

    added syntax changes

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,4 @@
 -- Write a script that creates a table called first_table in the current
 -- database
 -- coloumn ID and name
-CREATE TABLE IF NOT EXISTS first_table (
-       id int,
-       name VARCHAR9256),
-);
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
