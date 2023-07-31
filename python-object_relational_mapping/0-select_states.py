5c7e88d1aea99ed6ffa72503328407632a781d12
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-31 23:32:16 +1000

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,39 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""Script to list all states from the hbtn_0e_0_usa database"""
+
 import sys
 import MySQLdb
 
+def main():
+    # Check if all required arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
+    # Get MySQL connection details from command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to the MySQL server running on localhost at port 3306
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+
+    # Create a cursor to execute SQL queries
+    cursor = conn.cursor()
+
+    # Execute the query to fetch all states in ascending order by states.id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+
+    # Fetch all the rows from the result
+    rows = cursor.fetchall()
+
+    # Display the results as they are in the example
+    for row in rows:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    main()
