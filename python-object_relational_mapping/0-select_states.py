ed65a5718bd14575fcc08497d4a1ae183aeec388
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-31 23:34:32 +1000

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
-"""Script to list all states from the hbtn_0e_0_usa database"""
+"""Lists all states from the database"""
 
-import sys
 import MySQLdb
+from sys import argv
 
-def main():
-    # Check if all required arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
 
-    # Get MySQL connection details from command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+def get_state(username, password, db_name):
+    """ Lists all states from the database"""
+    db = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            port=3306,
+            db=db_name
+    )
+    cursor = db.cursor()
 
-    # Connect to the MySQL server running on localhost at port 3306
-    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
-
-    # Create a cursor to execute SQL queries
-    cursor = conn.cursor()
-
-    # Execute the query to fetch all states in ascending order by states.id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-
-    # Fetch all the rows from the result
+    cursor.execute("SELECT id, name FROM states ORDER BY id;")
     rows = cursor.fetchall()
-
-    # Display the results as they are in the example
     for row in rows:
         print(row)
-
-    # Close the cursor and connection
     cursor.close()
-    conn.close()
+    db.close()
+
 
 if __name__ == "__main__":
-    main()
+    get_state(argv[1], argv[2], argv[3])
