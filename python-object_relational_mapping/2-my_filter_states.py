1fec20a276f54b34e72e0c27cc1c350b8c695790
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-31 21:41:57 -0700

    argv[4]change

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         state_name=sys.argv[4])
+                         db=sys.argv[3])
+    state_name=sys.argv[4]
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
     WHERE name LIKE '{}';".format(state_name))
