55f5277c77f1983c6bf452c351cc9a6f79c36c9c
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-31 21:34:02 -0700

    added formatting according to required format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,9 +10,8 @@ if __name__ == "__main__":
                          db=sys.argv[3],
                          state_name=sys.argv[4])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER \
-    BY id ASC;".format(state_name)
-    cur.execute(query)
+    cur.execute("SELECT * FROM states \
+    WHERE name LIKE '{}';".format(state_name))
     rows = cur.fetchall()
     for row in rows:
         print(row)
