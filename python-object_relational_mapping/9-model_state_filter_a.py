260b2b9089e60519c0c5f0f6af9aea3c3c301ad3
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-01 01:03:19 -0700

    added task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter 'a' from the database hbtn_0e_6_usa
+Lists all State objects that contain t\
+he letter 'a' from the database hbtn_0e_6_usa
+Usage:
+    ./9-model_state_filter_a.py \
+<mysql_username> <mysql_password> <database_name>
 """
 
 import sys
@@ -8,43 +12,26 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
-    # Check if all required arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: ./9-model_state_filter_a.py /
-        <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
-
-    # Get MySQL connection details from command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to the MySQL server running on localhost at port 3306
+    # Connect to the database
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            mysql_username, mysql_password, database_name
-        ),
-        pool_pre_ping=True,
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
+        pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
 
-    # Bind the engine to the Base
-    Base.metadata.bind = engine
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    # Create a sessionmaker to interact with the database
-    DBSession = sessionmaker(bind=engine)
+    # Query the State objects that contain the letter 'a' in their names
+    states_with_a = session.query(State)\
+                           .filter(State.name.like('%a%'))\
+                           .order_by(State.id).all()
 
-    # Create a session
-    session = DBSession()
-
-    # Query all State objects that contain the letter 'a'
-    states_with_a = /
-    session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-
-    # Print the State objects in the required format
+    # Display the results
     for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
+        print(f"{state.id}: {state.name}")
 
     # Close the session
     session.close()
