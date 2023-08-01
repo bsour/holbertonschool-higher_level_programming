f94cf9877f0ebac8dbe2779aba7489870f33a406
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-01 14:47:31 +1000

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that takes an argument and dsiplays all values in states table """
+"""script that takes in an argument and displays all values in the states
+table that match the argument"""
 import MySQLdb
 import sys
 
@@ -8,10 +9,10 @@ if __name__ == "__main__":
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
-    state_name=sys.argv[4]
+    state_arg = sys.argv[4]
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
-    WHERE name LIKE '{}';".format(state_name))
+    WHERE name LIKE '{}';".format(state_arg))
     rows = cur.fetchall()
     for row in rows:
         print(row)
