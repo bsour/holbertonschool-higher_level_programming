b837c19ff8cfe46c645596ee665c070d09bd0b5e
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-07-31 21:23:09 -0700

    added task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script that takes an argument and dsiplays all values in states table """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         state_name=sys.argv[4])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+    WHERE name=%s ORDER BY id ASC;", (state_name,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
