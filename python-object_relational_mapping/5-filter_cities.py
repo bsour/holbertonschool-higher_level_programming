e6764da8c17eae04feb4b0dbeb8e182b974b7fbf
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-01 16:12:59 +1000

    Update 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
-""" Write a script that takes in the name of a state as an argument and lists \
-all cities of that state, using the database hbtn_0e_4_usa """
-import sys
+"""
+Takes in an argument and displays all values matching the argument
+and is injection proof.
+"""
+
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
+
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE BINARY states.name LIKE %s
+    ORDER BY cities.id;
+    """
+    name = (sys.argv[4] + '%',)
+
     cur = db.cursor()
-    query = "SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC;"
-    cur.execute(query, (state_name,))
+    cur.execute(query, name)
+
     rows = cur.fetchall()
+    cities = []
     for row in rows:
-        print(row[0])
+        cities.append(row[0])
+    print(', ' .join(cities))
+
     cur.close()
     db.close()
     
